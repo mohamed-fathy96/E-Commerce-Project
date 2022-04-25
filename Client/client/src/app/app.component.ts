7255d33b367f883ca4f4bf2a8674ea6a1f499e47
mohamed-fathy96 <pet.mohamedfathy@gmail.com>
2022-04-26 00:32:16 +0200

    added iproduct, ipagniation to client, added some styling

--- Client/client/src/app/app.component.ts
+++ Client/client/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IPagination } from './models/pagination';
+import {IProduct} from './models/product';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ECommerce';
+  products: IProduct[] = [];
+  constructor(private http:HttpClient) {
+   
+  }
+  ngOnInit(): void {
+    this.http.get<IPagination>('https://localhost:7105/api/Products').subscribe((response:IPagination) =>{
+      this.products = response.data;
+      console.log(this.products);   
+     })
+  }
+
 }
